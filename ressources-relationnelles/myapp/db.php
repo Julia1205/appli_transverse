8679d8eae3a07b64d643fba98b9993440d5d22e8
Julia1205 <92686656+Julia1205@users.noreply.github.com>
2023-04-27 13:29:59 +0200

    update .env etc

---
+++ ressources-relationnelles/myapp/db.php
@@ -0,0 +1,7 @@
+ app.baseURL = 'http://localhost'
+ app.forceGlobalSecureRequests = false
+ database.default.hostname = mysql
+ database.default.database = s_transverse
+ database.default.username = root
+ database.default.password = root
+ database.default.DBDriver = MySQLi
